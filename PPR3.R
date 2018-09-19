9135ec5d88b4c5449d651c5304c0f3e1c45ee8aa
deponent-verb <43394079+deponent-verb@users.noreply.github.com>
2018-09-19 15:28:28 +0930

    Add files via upload
    We can now prepare a batch of neutral simulations.

--- PPR3.R
+++ PPR3.R
@@ -2,8 +2,6 @@ library(phonTools)
 library(MASS)
 setwd("~/discoal/")
 
-num_pad_total = 200
-
 #set up discoal parameters
 mu=1.5e-8
 recomb_rate=1e-8
@@ -16,7 +14,18 @@ selection<-c(0,0.001,0.005,0.01,0.02,0.05,0.1,0.2,0.5)
 nrep=1
 nSites<-format(100000,scientific = FALSE)
 
-for (i in 1:10){
+######simulation code starts here!
+
+#initialization for function, make sure you clean these on each run
+
+haplo_list=list()
+segsites_vec=c()
+haplo_padded=list()
+num_sim=10
+
+#running simulations
+
+for (i in 1:num_sim){
   
   cmd = paste("~/discoal/discoal", sampleSize, nrep, nSites, "-t", theta, "-r", r)
   
@@ -25,20 +34,29 @@ for (i in 1:10){
   start <- which(substr(sim, 1,3) == "pos") + 1  # Searching for the line with "position" at the beginning
   end = length(sim)
   
-  segsites = as.numeric(gsub(pattern = "seg.* ", replacement = "", sim[substr(sim, 1,3) == "seg"]))  
+  segsites_vec = c(segsites_vec,as.numeric(gsub(pattern = "seg.* ", replacement = "", sim[substr(sim, 1,3) == "seg"])))  
   
-  haplo = sapply(sim[start:end], function(s) {as.numeric(strsplit(s, split="")[[1]])}, USE.NAMES = F)
-
-  num_pad = num_pad_total - segsites
-  
-  haplo_padded = rbind(haplo, zeros(x=num_pad, y=ncol(haplo)))
-  
-  #image(haplo_padded)
-  
-  file_name=paste("~/work/PPR3/data/neutral/sim", i, ".txt")
-  
-  write.matrix(t(haplo_padded), file =file_name)
-  
+  haplo_list[[i]] = sapply(sim[start:end], function(s) {as.numeric(strsplit(s, split="")[[1]])}, USE.NAMES = F)
 }
+
+#padding 
+
+num_pad_total=max(segsites_vec)
+for (i in 1:num_sim){
+num_pad = num_pad_total - segsites_vec[i]
+haplo_padded[[i]] = rbind(haplo_list[[i]], zeros(x=num_pad, y=ncol(haplo_list[[i]])))
+}
+
+#writing the files out
+for (i in 1:num_sim){
+file_name=paste("~/work/PPR3/data/neutral/sim", i, ".txt")
+write.matrix(t(haplo_padded), file =file_name)
+}
+
+#for checking functions
+segsites_vec
+haplo_list
+haplo_padded
+#image(haplo_padded)
 
 #save(), and load() to make it into R object
