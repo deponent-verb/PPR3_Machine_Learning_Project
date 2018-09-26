414f323f2aa2587a22cd0aea267fa864dc2bd00e
deponent-verb <43394079+deponent-verb@users.noreply.github.com>
2018-09-26 10:57:23 +0930

    Add files via upload

--- detection_ml.R
+++ detection_ml.R
@@ -2,6 +2,32 @@ library(keras)
 
 load(file="~/work/PPR3/processed_data/padded_haplotypes.Rdata")
 
+#partitioning data
+
+data<-list()
+data["samples"]<-list()
+data["labels"]<-list()
+
+##getting the neutral data and labelling
+
+  for (i in 1:length(haplo_padded[[1]])){
+    data[["samples"]][[i]]<-haplo_padded[[1]][[i]]
+    data[["labels"]][[i]]<-0
+  }
+
+##getting s=0.5 data
+
+push<-length(haplo_padded[[1]])
+for (i in push+1:push+push+length(haplo_padded[[9]])){
+  data[["samples"]][[i]]<-haplo_padded[[1]][[i]]
+  data[["labels"]][[i]]<-0
+}
+
+for(s in c(1,9)){
+  print(s)
+}
+
+
 #processing data into train + test
 
 train<-list()
